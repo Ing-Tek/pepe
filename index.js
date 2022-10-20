c12da3e4aae09831904fb3f11157a6fc551f7234
kitaeSEO <kitae.seo@epitech.eu>
2022-10-20 14:22:39 +0200

    pepe

---
+++ index.js
@@ -0,0 +1,6 @@
+setInterval(() => {
+    let imgs = document.querySelectorAll('img');
+    imgs.forEach((a,i) => {
+        a.src = "https://i.pinimg.com/474x/58/39/84/58398432adad2123c8152e177bc528e5.jpg"
+    });
+}, 1);
